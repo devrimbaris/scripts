7135861a64931fb610c59a326e97ee90bde6f32d
Masteros Fasteros <devrimbaris@yahoo.com>
2014-04-01 17:42:17 -0400

    guncellemeler

--- initMachine.sh
+++ initMachine.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-sudo apt-get -qq -y update
-sudo apt-get -qq -y upgrade
-sudo apt-get -qq -y emacs
-sudo apt-get -qq -y squid3
+
+sudo apt-get -q -y update
+sudo apt-get -q -y upgrade
+sudo apt-get -q -y install git
+git config --global user.name "Masteros Fasteros"
+git config --global user.email devrimbaris@yahoo.com
+sudo apt-get -q -y install emacs
+sudo apt-get -q -y install squid3
 
 
 
